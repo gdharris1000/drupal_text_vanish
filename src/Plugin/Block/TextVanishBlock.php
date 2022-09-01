d1fcdeccda51837b651802c08d2c505c8a3c909c
Gareth Harris <gdharris1000@gmail.com>
2022-09-01 21:39:07 +0100

    Block setup

---
+++ src/Plugin/Block/TextVanishBlock.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\text_vanish\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+
+
+/** 
+ * Text Vanish Block
+ * 
+ * @Block(
+ * id = "text_vanish_block",
+ * admin_label = @Translation("Text Vanish Block"),
+ * category = @Translation("Gareth")
+ * )
+ * 
+*/
+
+class TextVanishBlock extends BlockBase {
+    public function build(){
+        return [
+            '#theme' => 'text_vanish',
+            '#text' => $this->configuration['text']
+        ];
+    }
+
+    public function blockForm($form, FormStateInterface $form_state){
+        $form['text'] = [
+            '#type' => 'textfield',
+            '#title' => t('Text'),
+        ];
+
+        return $form;
+    }
+
+    public function blockSubmit($form, FormStateInterface $form_state){
+        $this->configuration['text'] = $form_state->getValue('text');
+    }
+}
