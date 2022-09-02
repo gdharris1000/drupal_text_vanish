e38beefbff666dadc9b3d0aaadaef5572ff3c92f
Gareth Harris <gdharris1000@gmail.com>
2022-09-02 07:07:19 +0100

    Split text in js

--- js/text_vanish.js
+++ js/text_vanish.js
@@ -1,1 +1,15 @@
-console.log("text-vanish");
+const element_text = document.getElementById("text-vanish-content");
+
+text_split = element_text.innerHTML.split("");
+
+text_split_ids = [];
+
+for (i = 0; i < text_split.length; i++) {
+  text_split_ids.push(
+    "<span id='text-split-" + i + "'>" + text_split[i] + "</span>"
+  );
+}
+
+element_text.innerHTML = text_split_ids;
+
+console.log(element_text.innerHTML);
