a795fd89a8ea27a518d117f387442eeface3b607
Gareth Harris <gdharris1000@gmail.com>
2022-09-02 21:03:53 +0100

    Make characters disapper one by one

--- js/text_vanish.js
+++ js/text_vanish.js
@@ -10,6 +10,22 @@ for (i = 0; i < text_split.length; i++) {
   );
 }
 
-element_text.innerHTML = text_split_ids;
+element_text.innerHTML = text_split_ids.join("");
 
 console.log(element_text.innerHTML);
+
+var counter = 0;
+
+setInterval(function () {
+  if (counter < text_split.length) {
+    let element_id = "text-split-" + counter;
+    element = document.getElementById(element_id);
+    hideText(element);
+    counter++;
+  }
+}, 1000);
+
+function hideText(element) {
+  element.innerHTML = " ";
+  console.log(element.innerHTML);
+}
